6d86192d85878f17c54d9d8c3430c88fbb531df2
Kosh <mail@ex.com>
2022-07-14 22:05:02 +0300

    Task47 done

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -3,3 +3,37 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+Console.Clear();
+double[,] NewMatrixRnd(int row, int column, int min, int max)
+{
+    double[,] matrix = new double[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * ((max - min) + min), 4);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0)
+                Console.Write("|");
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j], 3}| ");
+            else
+                Console.Write($"{matrix[i, j], 3}|");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(NewMatrixRnd(3, 4, -100, 100));
