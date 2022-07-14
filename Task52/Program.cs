153675ac50659480b62161151b7d72de8d829cfc
Kosh <mail@ex.com>
2022-07-14 22:22:52 +0300

    Task52 started

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -4,3 +4,37 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+int[,] NewMatrixRnd(int row, int column, int min, int max)
+{
+    int[,] matrix = new int[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0)
+                Console.Write("|");
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j], 3}| ");
+            else
+                Console.Write($"{matrix[i, j], 3}|");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(NewMatrixRnd(3, 4, -100, 100));
